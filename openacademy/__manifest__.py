ef071427a76605a3e22afbca34e4f81cc99b7a64
Kaliuta <jumpvampir@gmail.com>
2022-03-30 22:42:23 +0300

    Manifest

--- openacademy/__manifest__.py
+++ openacademy/__manifest__.py
@@ -1,23 +1,11 @@
 {
     'name': "openacademy",
-
-    'summary': """
-        Open Academy""",
-
-    'description': """
-        Open Academy module
-    """,
-
+    'summary': "Open Academy - навчальний модуль з курсу odoo-lerning.",
+    'version': '15.0.1.0.1',
+    'description': """Open Academy - навчальний модуль з курсу odoo-lerning.""",
     'author': "Vasyl Kaliuta",
     'website': "https://borove.top",
-
-    # Categories can be used to filter modules in modules listing
-    # Check https://github.com/odoo/odoo/blob/15.0/odoo/addons/base/data/ir_module_category_data.xml
-    # for the full list
     'category': 'Uncategorized',
-    'version': '15.0.1.0.1',
-
-    # any module necessary for this one to work correctly
     'depends': ['base', 'board'],
     'application': True,
 
