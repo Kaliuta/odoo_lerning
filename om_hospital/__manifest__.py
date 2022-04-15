7fba5dae365002c30c1dbbdf974d794c17e282df
Kaliuta <jumpvampir@gmail.com>
2022-04-15 12:14:46 +0300

    19. Search Panel

--- om_hospital/__manifest__.py
+++ om_hospital/__manifest__.py
@@ -4,7 +4,7 @@
     'name': 'Hospital Management',
     'version': '1.0.0',
     'category': 'Hospital',
-    'author': "Vasyl Kaliutaa",
+    'author': "Vasyl Kaliuta",
     'sequence': -100,
     'summary': 'Hospital management system',
     'description': """Hospital management system module""",
