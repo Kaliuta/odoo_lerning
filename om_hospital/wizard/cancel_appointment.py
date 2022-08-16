be53cf7bd77b884304309f9551be4f5af761acfe
Kaliuta <jumpvampir@gmail.com>
2022-08-16 12:30:55 +0300

    81. Apply Domain For Fields In Odoo

--- om_hospital/wizard/cancel_appointment.py
+++ om_hospital/wizard/cancel_appointment.py
@@ -14,7 +14,8 @@ class CancelAppointmentWizard(models.TransientModel):
             res['appointment_id'] = self.env.context.get('active_id')
         return res
 
-    appointment_id = fields.Many2one('hospital.appointment', string='Appointment')
+    appointment_id = fields.Many2one('hospital.appointment', string='Appointment',
+                                     domain=[('state', '=', 'draft'), ('priority', 'in', ('0', '1', False))])
     reason = fields.Text(string='Reason')
     date_cancel = fields.Date(string='Cancellation Date')
 
