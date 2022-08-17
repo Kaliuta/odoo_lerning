09de18a6f28d07054ad533a9beb508e81cdd0747
Kaliuta <jumpvampir@gmail.com>
2022-08-17 16:18:38 +0300

    82. Sql Constraints In Odoo

--- om_hospital/models/patient_tag.py
+++ om_hospital/models/patient_tag.py
@@ -8,3 +8,9 @@ class PatientTag(models.Model):
     active = fields.Boolean(string="Active", default=True)
     color = fields.Integer(string="Color")
     color_2 = fields.Char(string="Color 2")
+    sequence = fields.Integer(string="Sequence")
+
+    _sql_constraints = [
+        ('unique_tag_name', 'unique (name, active)', 'Name must be unique.'),
+        ('check_sequence', 'check(sequence > 0)', 'Sequence must be non zero possitive number.')
+    ]
