b18294c18bd928738321d0eb9f4d8c99a54a51a2
Kaliuta <jumpvampir@gmail.com>
2023-03-15 12:35:36 +0200

    115. Log Access Model Attribute

---
+++ om_hospital/models/operation.py
@@ -0,0 +1,9 @@
+from odoo import api, fields, models, _
+
+
+class HospitalOperation(models.Model):
+    _name = "hospital.operation"
+    _description = "Hospital Operation"
+    _log_access = False
+
+    doctor_id = fields.Many2one('res.user', string="Doctor")
